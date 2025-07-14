bfd4f745d5e7c5a36389529d3924ec98626e6d31
Long Nguyen <faker2407@gmail.com>
2025-07-14 13:44:44 +0700

    get status, protocol and pretty print response header

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Hello World!\n")
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Printf("Status: %s\n", resp.Status)
+	fmt.Printf("Protocol: %s\n", resp.Proto)
+	fmt.Print("Header:")
+	for key, values := range resp.Header {
+		fmt.Printf("%s: %s\n", key, strings.Join(values, ", "))
+	}
+	defer resp.Body.Close()
+
 }
